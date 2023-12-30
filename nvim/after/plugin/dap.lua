a8aec48976866edb685c720bf311eef001f09c04
Patrick Muehlbauer <patrick@opentrash.org>
2023-12-30 17:59:37 +0100

    nvim: dap python ui

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,8 +1,14 @@
-require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+local dap = require('dap')
+
+require("dapui").setup()
+require("dap-python").setup()
 require("dap-python").test_runner = "pytest"
+
+vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({ reset = true })<CR>", { noremap = true })
 
 vim.keymap.set("n", "<leader>dn", ":lua require('dap-python').test_method()<CR>")
 vim.keymap.set("n", "<leader>df", ":lua require('dap-python').test_class()<CR>")
 vim.keymap.set("n", "<leader>ds", ":lua require('dap-python').debug_selection()<CR>")
-
-require("dapui").setup()
