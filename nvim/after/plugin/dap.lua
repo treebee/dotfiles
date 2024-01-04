d579ca46491a43e85bfa4e381daf932de35ce70c
Patrick Mühlbauer <pam@solute.de>
2024-01-04 08:00:59 +0100

    nvim/dap: Add shortcut for dapui close

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -8,6 +8,7 @@ vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true 
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({ reset = true })<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dR", ":lua require('dapui').close({ reset = true })<CR>", { noremap = true })
 
 vim.keymap.set("n", "<leader>dn", ":lua require('dap-python').test_method()<CR>")
 vim.keymap.set("n", "<leader>df", ":lua require('dap-python').test_class()<CR>")
