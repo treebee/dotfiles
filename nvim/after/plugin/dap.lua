ccbb994daa3eab846c9b0b4355568bc5e533da5d
Patrick Mühlbauer <pam@solute.de>
2024-01-04 10:41:13 +0100

    nvim/dap: Change dapui layout

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,6 +1,6 @@
 local dap = require('dap')
+local dapui = require('dapui')
 
-require("dapui").setup()
 require("dap-python").setup()
 require("dap-python").test_runner = "pytest"
 
@@ -13,3 +13,31 @@ vim.api.nvim_set_keymap("n", "<leader>dR", ":lua require('dapui').close({ reset 
 vim.keymap.set("n", "<leader>dn", ":lua require('dap-python').test_method()<CR>")
 vim.keymap.set("n", "<leader>df", ":lua require('dap-python').test_class()<CR>")
 vim.keymap.set("n", "<leader>ds", ":lua require('dap-python').debug_selection()<CR>")
+
+
+dapui.setup({
+    layouts = {
+        {
+            -- You can change the order of elements in the sidebar
+            elements = {
+                -- Provide IDs as strings or tables with "id" and "size" keys
+                {
+                    id = "scopes",
+                    size = 0.25, -- Can be float or integer > 1
+                },
+                { id = "breakpoints", size = 0.25 },
+                { id = "stacks",      size = 0.25 },
+                { id = "watches",     size = 0.25 },
+            },
+            size = 40,
+            position = "left", -- Can be "left" or "right"
+        },
+        {
+            elements = {
+                "console",
+            },
+            size = 20,
+            position = "bottom", -- Can be "bottom" or "top"
+        },
+    },
+})
