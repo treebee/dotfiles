c73dc51bdcc5083225ffb1fe8a5fe13dee8c2062
Patrick Mühlbauer <patrick@opentrash.org>
2024-04-14 22:23:38 +0200

    wezterm: Enable font ligatures

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,6 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font 'Monaspace Krypton'
+config.font = wezterm.font({ family = 'Monaspace Krypton', harfbuzz_features = { 'calt', 'liga', 'dlig', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' }, })
 -- and finally, return the configuration to wezterm
 return config
