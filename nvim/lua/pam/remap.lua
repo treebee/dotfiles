8f5c3e6978081ed8f8cf99fd358d40232da66c00
Patrick Mühlbauer <patrick@opentrash.org>
2024-12-18 18:29:18 +0100

    nvim: Add mappings for navigating quickfix list

--- nvim/lua/pam/remap.lua
+++ nvim/lua/pam/remap.lua
@@ -31,8 +31,8 @@ vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end)
 
---vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-m>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
