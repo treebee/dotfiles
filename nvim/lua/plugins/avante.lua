fa38d49edf13f037e1b65e1c6c91cf00b90f2cf3
Patrick Mühlbauer <pam@opentrash.org>
2025-06-11 08:45:15 +0200

    nvim/avante: Fix and update config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -6,12 +6,14 @@ return {
         -- add any opts here
         -- for example
         provider = "gemini",
-        openai = {
-            model = "gemini-2.5-pro-preview-05-06", -- your desired model (or use gpt-4o, etc.)
-            timeout = 30000,                        -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0,
-            max_completion_tokens = 8192,           -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        providers = {
+            gemini = {
+                model = "gemini-2.5-pro-preview-05-06", -- your desired model (or use gpt-4o, etc.)
+                timeout = 30000,                    -- Timeout in milliseconds, increase this for reasoning models
+                temperature = 0,
+                max_completion_tokens = 8192,       -- Increase this to include reasoning tokens (for reasoning models)
+                --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            },
         },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
