562a56f11818491838a2a9a2c2112ba504ce73ee
Patrick Mühlbauer <patrick@opentrash.org>
2025-11-19 20:46:36 +0100

    nvim: Fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
-	lazy = false,
-	build = ":TSUpdate",
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local ts_install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter"
+        vim.opt.runtimepath:append(ts_install_dir .. "/runtime")
+    end,
 }
