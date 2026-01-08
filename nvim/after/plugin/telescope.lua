13ad5019b3283cc352a1391e96f08295b399e299
Patrick Mühlbauer <pam@solute.de>
2026-01-08 07:32:02 +0100

    nvim/telescope: Readd grep keymap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find tracked files' })
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
