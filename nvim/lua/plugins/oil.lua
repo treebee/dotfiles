e506289447682d454abdd619fdffc57afada8b87
Patrick Mühlbauer <pam@solute.de>
2026-07-03 11:56:50 +0200

    nvim/oil: Show hidden files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,15 +1,19 @@
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {
-	keymaps = {
-		["<C-p>"] = false
-	}
-  },
-  -- Optional dependencies
-  -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        keymaps = {
+            ["<C-p>"] = false
+        },
+        view_options = {
+            show_hidden = true,
+        }
+
+    },
+    -- Optional dependencies
+    -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
 }
